3abfa29ed90d0426af98386a4006d11aedccbaf2
Wen Bin <wenbin_ng@yahoo.com.sg>
2023-08-22 23:26:02 +0800

    chore: completed ex 1

---
+++ 02-write-your-first-program/first-go/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// excercise 1
+	fmt.Println("My name is Bing")
+	fmt.Println("My best friend name is Chandler")
+	fmt.Print("string1", "string2") // No space added, raw string
+	fmt.Print("string1", 1, 2)      // Space is added for non strings
+
+}
